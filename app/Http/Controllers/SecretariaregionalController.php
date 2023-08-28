c8e3c44f4178579f8da0e7563d0e0329cd544a01
miguelborda <miguelangelbg20@gmail.com>
2023-08-28 00:55:05 -0400

    3er creando modelsimple

---
+++ app/Http/Controllers/SecretariaregionalController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Secretariaregional;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SecretariaregionalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Secretariaregional  $secretariaregional
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Secretariaregional $secretariaregional)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Secretariaregional  $secretariaregional
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Secretariaregional $secretariaregional)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Secretariaregional  $secretariaregional
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Secretariaregional $secretariaregional)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Secretariaregional  $secretariaregional
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Secretariaregional $secretariaregional)
+    {
+        //
+    }
+}
